958e6561ef8fdcf93db846b16a12d571e5fecdca
oscaroneim <oscar4londres@gmail.com>
2025-02-28 19:48:06 +0000

    trying to fix lint but not

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,18 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import tsParser from "@typescript-eslint/parser";
 
-
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { parser: tsParser, globals: globals.browser }},
-  {rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "semi": ["error", "always"],
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parser: tsParser, globals: globals.browser } },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      semi: ["error", "always"],
+    },
   },
-},
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
