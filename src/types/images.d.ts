98ab96632846f0481eb84bc14cdffa4599b08f97
oscaroneim <oscar4londres@gmail.com>
2025-03-23 11:40:09 +0000

    fixing more linting errors

---
+++ src/types/images.d.ts
@@ -0,0 +1,26 @@
+// types/images.d.ts
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.avif" {
+  const value: string;
+  export default value;
+}
